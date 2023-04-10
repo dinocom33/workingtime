b1d9b4673c95b715fcdab9296bf86cd87ce0e946
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-04-10 15:05:53 +0300

    Some new apps

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import team, add_team
+from .views import team, add_team, edit_team, activate_team
 
 app_name = "team"
 
 urlpatterns = [
     path("add_team/", add_team, name="add_team"),
+    path("edit_team/", edit_team, name="edit_team"),
+    path("activate_team/<int:team_id>", activate_team, name="activate_team"),
     path("<int:team_id>/", team, name="team"),
 ]
--- apps/team/views.py
+++ apps/team/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Team
 from django.contrib.auth.decorators import login_required
@@ -8,6 +9,35 @@ def team(request, team_id):
     team = get_object_or_404(Team, pk=team_id, status=Team.ACTIVE, members__in=[request.user])
 
     return render(request, "team/team.html", {'team': team})
+
+
+@login_required
+def activate_team(request, team_id):
+    team = get_object_or_404(Team, pk=team_id, status=Team.ACTIVE, members__in=[request.user])
+    userprofile = request.user.userprofile
+    userprofile.active_team_id = team_id
+    userprofile.save()
+
+    messages.info(request, "The team has been activated")
+
+    return redirect("team:team", team_id=team_id)
+
+
+@login_required
+def edit_team(request):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE, members__in=[request.user])
+    if request.method == 'POST':
+        title = request.POST.get("title")
+
+        if title:
+            team.title = title
+            team.save()
+
+            messages.info(request, "The changes have been saved successfully")
+
+            return redirect("team:team", team_id=team.id)
+
+    return render(request, "team/edit_team.html", {"team": team})
 
 
 @login_required
