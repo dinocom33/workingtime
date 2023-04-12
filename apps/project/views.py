3f8d54c2ff6c9f8965a149c3bfe3e7a5e2db7d18
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-04-12 17:09:04 +0300

    Some new apps

--- apps/project/views.py
+++ apps/project/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .models import Project
+from .models import Project, Task
 from apps.team.models import Team
 
 
@@ -29,7 +29,20 @@ def project(request, project_id):
     team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
     project = get_object_or_404(Project, team=team, pk=project_id)
 
-    return render(request, "project/project.html", {"team": team, "project": project})
+    if request.method == 'POST':
+        title = request.POST.get('title')
+
+        if title:
+            task = Task.objects.create(team=team, project=project, created_by=request.user, title=title)
+
+            messages.info(request, 'Task was created successfully')
+
+            return redirect("project:project", project_id=project.id)
+
+    tasks_todo = project.tasks.filter(status=Task.TODO)
+    tasks_done = project.tasks.filter(status=Task.DONE)
+
+    return render(request, "project/project.html", {"team": team, "project": project, "tasks_todo": tasks_todo, "tasks_done": tasks_done})
 
 
 @login_required()
@@ -49,3 +62,32 @@ def edit_project(request, project_id):
             return redirect("project:project", project_id=project.id)
 
     return render(request, "project/edit_project.html", {"team": team, "project": project})
+
+
+@login_required
+def task(request, project_id, task_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id, team=team)
+
+    return render(request, "project/task.html", {"team": team, "project": project, "task": task})
+
+
+@login_required()
+def edit_task(request, project_id, task_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
+    project = get_object_or_404(Project, team=team, pk=project_id)
+    task = get_object_or_404(Task, pk=task_id, team=team)
+
+    if request.method == "POST":
+        title = request.POST.get('title')
+
+        if title:
+            task.title = title
+            task.save()
+
+            messages.info(request, "Task was saved successfully")
+
+            return redirect('project:task', project_id=project_id, task_id=task_id)
+
+    return render(request, "project/edit_task.html", {"team": team, "project": project, "task": task})
