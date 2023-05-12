4947f4dfefc5b4870befb8dd734bb7c841392922
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-05-12 15:25:00 +0300

    Add new functionality

--- apps/project/api.py
+++ apps/project/api.py
@@ -30,6 +30,8 @@ def api_stop_timer(request):
 
     if tracked_minutes < 1:
         tracked_minutes = 1
+    elif tracked_minutes > 1:
+        tracked_minutes += 1
 
     entry.minutes = tracked_minutes
     entry.is_tracked = False
